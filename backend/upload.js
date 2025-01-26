82061dc3f507464ab109e09682358aee253c3271
Shavi Sharma <mail4shavi@gmail.com>
2025-01-26 17:51:37 +0530

    everything is fine

--- backend/upload.js
+++ backend/upload.js
@@ -1,30 +1,25 @@
 const mongoose = require("mongoose");
-const connect = require("./connect"); // Import your connect function
-const Question = require("./schema"); // Import the Question model
+const connect = require("./connect"); 
+const Question = require("./schema"); 
 const fs = require("fs");
 
 async function uploadData() {
     try {
-      // Connect to MongoDB
       connect();
   
-      // Read and parse the JSON file
       const data = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
   
-      // Convert `_id` and `siblingId` to ObjectId if they exist
       const questions = data.map((q) => ({
         ...q,
         _id: q._id?.$oid ? new mongoose.Types.ObjectId(q._id.$oid) : new mongoose.Types.ObjectId(q._id),
         siblingId: q.siblingId?.$oid ? new mongoose.Types.ObjectId(q.siblingId.$oid) : new mongoose.Types.ObjectId(q.siblingId),
       }));
-  
-      // Insert data into the collection
       await Question.insertMany(questions);
       console.log("Data uploaded successfully!");
-      process.exit(); // Exit the process after uploading
+      process.exit(); 
     } catch (err) {
       console.error("Error uploading data:", err.message);
-      process.exit(1); // Exit with failure
+      process.exit(1); 
     }
   }
   
